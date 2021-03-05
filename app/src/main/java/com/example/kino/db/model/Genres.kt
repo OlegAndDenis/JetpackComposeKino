7cc840605f5f66763731af4e000109d50dd7652e
Oleg <olegmojegov@yandex.ru>
2021-03-05 19:55:39 +0300

    Add param in table Genres

--- app/src/main/java/com/example/kino/db/model/Genres.kt
+++ app/src/main/java/com/example/kino/db/model/Genres.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "genres")
+@Entity(tableName = "genres", primaryKeys = ["idGenres", "id"])
 data class Genres(
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Long = 0,
     @ColumnInfo(name = "idGenres") val idGenres: Int,
