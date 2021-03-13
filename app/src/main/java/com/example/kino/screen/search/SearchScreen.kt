f145ad9707f92f75b3e0826f7e4742f14693de9f
Oleg <olegmojegov@yandex.ru>
2021-03-13 23:48:47 +0300

    Add searchFragment

---
+++ app/src/main/java/com/example/kino/screen/search/SearchScreen.kt
@@ -0,0 +1,28 @@
+package com.example.kino.screen.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kino.comonnscreen.BaseFragment
+import com.example.kino.di.components.FragmentComponent
+
+class SearchScreen: BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getFragmentComponent(): FragmentComponent {
+        TODO("Not yet implemented")
+    }
+
+}
