dade534c017f9b5fda561447a10b9cdc96b89dcc
Oleg <olegmojegov@yandex.ru>
2021-05-01 14:11:27 +0300

    Add method selectedTopFive in MovieViewModel

--- app/src/main/java/com/example/kino/screen/movie/MovieViewModel.kt
+++ app/src/main/java/com/example/kino/screen/movie/MovieViewModel.kt
@@ -6,12 +6,10 @@ import androidx.lifecycle.ViewModel
 import com.example.kino.db.DatabaseRepository
 import com.example.kino.db.model.Genres
 import com.example.kino.network.NetworkRepository
-import com.example.kino.network.model.movie.Movie
 import com.example.kino.network.model.movie.MovieResult
 import com.example.kino.plusAssign
 import com.example.kino.screen.common.TypeEnum
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
 import java.util.*
@@ -37,14 +35,16 @@ class MovieViewModel(
     private fun getPopulate(page: Int) {
         disposable += mNetworkRepository.getFilm(page)
             .subscribeOn(Schedulers.io())
-            .map {
-                val list = it.result.toMutableList()
-                list.sortByDescending { it.voteCount }
-                val topFiv = list.take(5).toMutableList()
-                Collections.rotate(topFiv, -3)
-                topFiv
-            }
+            .map {selectedTopFive(it.result)}
             .subscribe(resultMovie::postValue, Timber::e)
+    }
+
+    private fun selectedTopFive(list: List<MovieResult>): List<MovieResult> {
+        val editList = list.toMutableList()
+        editList.sortByDescending { it.voteCount }
+        val topFiv = editList.take(5).toMutableList()
+        Collections.rotate(topFiv, -3)
+       return topFiv
     }
 
     private fun getGenres() {
