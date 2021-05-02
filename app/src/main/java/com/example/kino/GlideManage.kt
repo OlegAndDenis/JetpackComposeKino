9c21365c6309d015e551542d87a587b59890cec6
Oleg <olegmojegov@yandex.ru>
2021-05-02 15:59:49 +0300

    Replace glide manager

--- app/src/main/java/com/example/kino/GlideManage.kt
+++ app/src/main/java/com/example/kino/GlideManage.kt
@@ -1,25 +1,37 @@
 package com.example.kino
 
+import android.content.Context
 import android.net.Uri
+import android.view.View
 import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
-fun RequestManager.loadBackdropPath(patch: String, containerImage: ImageView) {
-    load(Uri.parse("https://image.tmdb.org/t/p/w500/$patch"))
-        .into(containerImage)
-}
+class GlideManage private constructor(private val requestManager: RequestManager) {
 
-fun RequestManager.loadResources(containerImage: ImageView) {
-    load("file:///android_asset/img/ic_launcher_round.png")
-        .into(containerImage)
-}
+    companion object {
+        fun with(view: View): GlideManage = GlideManage(Glide.with(view))
 
-fun RequestManager.loadVarrags(paths: List<String>, vararg imageView: ImageView) {
-    paths.forEachIndexed { index, s ->
-        load(Uri.parse("https://image.tmdb.org/t/p/w500/$s"))
-            .centerCrop()
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .into(imageView[index])
+        fun with(context: Context): GlideManage = GlideManage(Glide.with(context))
     }
+
+    fun loadImage(path: String, imageView: ImageView) {
+        requestManager
+            .load(createPath(path))
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(imageView)
+    }
+
+    fun loadImage(paths: List<String>, vararg imageView: ImageView) {
+        paths.forEachIndexed { index, path ->
+            requestManager
+                .load(createPath(path))
+                .centerCrop()
+                .into(imageView[index])
+        }
+    }
+
+    private fun createPath(path: String): Uri =
+        Uri.parse("https://image.tmdb.org/t/p/w500/$path")
 }
--- app/src/main/java/com/example/kino/screen/movie/MovieViewHolder.kt
+++ app/src/main/java/com/example/kino/screen/movie/MovieViewHolder.kt
@@ -3,12 +3,10 @@ package com.example.kino.screen.movie
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.bumptech.glide.Glide
+import com.example.kino.GlideManage
 import com.example.kino.R
 import com.example.kino.adapter.holder.BindHolder
-import com.example.kino.adapter.holder.ClickItem
 import com.example.kino.databinding.MovieViewHolderLayoutBinding
-import com.example.kino.loadBackdropPath
 import com.example.kino.network.model.movie.MovieResult
 
 class MovieViewHolder private constructor(itemView: View, private val onClick: (Int) -> Unit) :
@@ -21,8 +19,8 @@ class MovieViewHolder private constructor(itemView: View, private val onClick: (
             .inflate(R.layout.movie_view_holder_layout, parent, false), onClick)
 
     override fun bind(item: MovieResult, position: Int) {
-        Glide.with(itemView)
-            .loadBackdropPath(item.backdropPath, binding.moviePopularity)
+        GlideManage.with(itemView)
+            .loadImage(item.backdropPath, binding.moviePopularity)
         binding.movieTitle.text = item.title
     }
 
