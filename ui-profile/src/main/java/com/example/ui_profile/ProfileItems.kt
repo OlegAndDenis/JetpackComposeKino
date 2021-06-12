648171356f90beb5d9adcc8a6acedd1b08ab8d4e
Денис Бекк <denisbeck555@gmail.com>
2021-06-13 00:34:33 +0300

    Add ProfileItems.kt

---
+++ ui-profile/src/main/java/com/example/ui_profile/ProfileItems.kt
@@ -0,0 +1,9 @@
+package com.example.ui_profile
+
+internal val profileItems = listOf(
+    ProfileItem(R.drawable.ic_profile_user, "Account"),
+    ProfileItem(R.drawable.ic_profile_notifications, "Notifications"),
+    ProfileItem(R.drawable.ic_profile_settings, "Settings"),
+    ProfileItem(R.drawable.ic_profile_help, "Help"),
+    ProfileItem(R.drawable.ic_profile_logout, "Logout"),
+)
