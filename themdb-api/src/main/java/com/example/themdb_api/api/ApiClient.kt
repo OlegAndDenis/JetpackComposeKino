8703d5331df11f67f906c9f3de39864685c2410f
OlegM <olegmojegov@yandex.ru>
2021-06-13 22:52:04 +0300

    Add api for themdb

--- themdb-api/src/main/java/com/example/themdb_api/api/ApiClient.kt
+++ themdb-api/src/main/java/com/example/themdb_api/api/ApiClient.kt
@@ -1,4 +1,13 @@
 package com.example.themdb_api.api
 
+import com.example.themdb_api.GenresApi
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface ApiClient {
+
+    @GET("genre/{type}/list?")
+    suspend fun getGenres(@Path("type") type : String, @Query("api_key") api_key: String,
+                  @Query("language") language: String): GenresApi
 }
