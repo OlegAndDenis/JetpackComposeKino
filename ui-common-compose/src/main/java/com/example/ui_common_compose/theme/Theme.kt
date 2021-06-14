f92c439aa3198d2009f32fb4ba9fc5e9219ceee5
Денис Бекк <denisbeck555@gmail.com>
2021-06-14 13:08:01 +0300

    Update Theme.kt
    Added DarkColors.

--- ui-common-compose/src/main/java/com/example/ui_common_compose/theme/Theme.kt
+++ ui-common-compose/src/main/java/com/example/ui_common_compose/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.example.ui_common_compose.theme
 
+import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
@@ -12,4 +13,15 @@ private val LightColors = lightColors(
     surface = SmokyWhite,
     onBackground = WetAsphalt,
     onSurface = WetAsphalt
+)
+
+private val DarkColors = darkColors(
+    primary = Color.Black,
+    onPrimary = Color.White,
+    secondary = ShinyOrange,
+    onSecondary = Color.Black,
+    background = Color.Black,
+    surface = Color.Black,
+    onBackground = Color.White,
+    onSurface = Color.White
 )
