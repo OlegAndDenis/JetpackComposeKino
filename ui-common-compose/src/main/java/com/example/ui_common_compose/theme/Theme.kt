3b65313e2f6c63e976dc01e1415a175101371824
Денис Бекк <denisbeck555@gmail.com>
2021-06-14 13:05:23 +0300

    Add Theme.kt
    Added LightColors.

---
+++ ui-common-compose/src/main/java/com/example/ui_common_compose/theme/Theme.kt
@@ -0,0 +1,15 @@
+package com.example.ui_common_compose.theme
+
+import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
+
+private val LightColors = lightColors(
+    primary = Color.White,
+    onPrimary = Color.Black,
+    secondary = ShinyOrange,
+    onSecondary = Color.White,
+    background = SmokyWhite,
+    surface = SmokyWhite,
+    onBackground = WetAsphalt,
+    onSurface = WetAsphalt
+)
