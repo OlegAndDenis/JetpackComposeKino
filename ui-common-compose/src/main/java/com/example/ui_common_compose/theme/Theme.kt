2422a362a9ecd9023a00b358ff2b6bafc5e5c8a7
Денис Бекк <denisbeck555@gmail.com>
2021-06-14 13:09:01 +0300

    Update Theme.kt
    Added KinoTheme.

--- ui-common-compose/src/main/java/com/example/ui_common_compose/theme/Theme.kt
+++ ui-common-compose/src/main/java/com/example/ui_common_compose/theme/Theme.kt
@@ -1,7 +1,10 @@
 package com.example.ui_common_compose.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val LightColors = lightColors(
@@ -25,3 +28,15 @@ private val DarkColors = darkColors(
     onBackground = Color.White,
     onSurface = Color.White
 )
+
+@Composable
+fun KinoTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkColors else LightColors,
+        typography = KinoTypography,
+        content = content
+    )
+}
