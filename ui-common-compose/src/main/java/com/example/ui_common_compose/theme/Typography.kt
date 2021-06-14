6b5b689ac4d85e1b6af1ab23bee723348248232e
Денис Бекк <denisbeck555@gmail.com>
2021-06-14 12:16:49 +0300

    Add Gotham FontFamily

--- ui-common-compose/src/main/java/com/example/ui_common_compose/theme/Typography.kt
+++ ui-common-compose/src/main/java/com/example/ui_common_compose/theme/Typography.kt
@@ -1,1 +1,11 @@
 package com.example.ui_common_compose.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.ui_common_compose.R
+
+val Gotham = FontFamily(
+    Font(R.font.gotham_bold),
+    Font(R.font.gotham_medium),
+    Font(R.font.gotham_book),
+)
