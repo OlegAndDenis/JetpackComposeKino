3d7f249df7d77d3dae56d16a141ed96618ba7891
Денис Бекк <denisbeck555@gmail.com>
2021-06-14 12:56:45 +0300

    Update Color.kt
    Changed visibility modifiers from private to
    public.

--- ui-common-compose/src/main/java/com/example/ui_common_compose/theme/Color.kt
+++ ui-common-compose/src/main/java/com/example/ui_common_compose/theme/Color.kt
@@ -2,9 +2,9 @@ package com.example.ui_common_compose.theme
 
 import androidx.compose.ui.graphics.Color
 
-private val ShinyOrange  = Color(0xFFFFBB3B)
-private val DarkTangerine  = Color(0xFFFFA90A)
-private val Silver  = Color(0xFFBFBFBF)
-private val PearlLightGray   = Color(0xFF999999)
-private val Gray  = Color(0xFF7E7E7E)
-private val WetAsphalt  = Color(0xFF5A5A5A)
+val ShinyOrange  = Color(0xFFFFBB3B)
+val DarkTangerine  = Color(0xFFFFA90A)
+val Silver  = Color(0xFFBFBFBF)
+val PearlLightGray   = Color(0xFF999999)
+val Gray  = Color(0xFF7E7E7E)
+val WetAsphalt  = Color(0xFF5A5A5A)
