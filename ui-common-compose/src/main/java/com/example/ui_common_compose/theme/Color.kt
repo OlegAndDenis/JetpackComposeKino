2b181853c636fe2f2608e0ce47dc0a4d78b8fec9
Денис Бекк <denisbeck555@gmail.com>
2021-06-14 13:03:37 +0300

    Update Color.kt
    Added SmokyWhite color.

--- ui-common-compose/src/main/java/com/example/ui_common_compose/theme/Color.kt
+++ ui-common-compose/src/main/java/com/example/ui_common_compose/theme/Color.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 val ShinyOrange  = Color(0xFFFFBB3B)
 val DarkTangerine  = Color(0xFFFFA90A)
+val SmokyWhite   = Color(0xFFFAFAFA)
 val Silver  = Color(0xFFBFBFBF)
 val PearlLightGray   = Color(0xFF999999)
 val Gray  = Color(0xFF7E7E7E)
