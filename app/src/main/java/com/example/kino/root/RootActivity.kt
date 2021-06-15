550581c1faff99716c8abcc2156c81dff6fa41ea
Денис Бекк <denisbeck555@gmail.com>
2021-06-15 20:04:33 +0300

    Update RootActivity.kt
    Added KinoTheme block.

--- app/src/main/java/com/example/kino/root/RootActivity.kt
+++ app/src/main/java/com/example/kino/root/RootActivity.kt
@@ -3,6 +3,7 @@ package com.example.kino.root
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.fragment.app.FragmentActivity
+import com.example.ui_common_compose.theme.KinoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,6 +11,10 @@ class RootActivity: FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {  }
+        setContent { 
+            KinoTheme() {
+                
+            }
+        }
     }
 }
