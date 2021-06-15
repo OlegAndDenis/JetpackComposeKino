f79f174e832b1f8f41b5ad71eca31762e251c56e
Денис Бекк <denisbeck555@gmail.com>
2021-06-15 20:19:22 +0300

    Add Root.kt

---
+++ app/src/main/java/com/example/kino/root/Root.kt
@@ -0,0 +1,23 @@
+package com.example.kino
+
+import AppNavigation
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.example.kino.extensions.currentRoute
+import com.example.kino.navigation.BottomNavBar
+import com.example.kino.navigation.blackList
+
+@Composable
+fun Root() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            if (!blackList.contains(navController.currentRoute())) {
+                BottomNavBar(navController)
+            }
+        }
+    ) { innerPadding ->
+        AppNavigation(navController = navController, paddings = innerPadding)
+    }
+}
