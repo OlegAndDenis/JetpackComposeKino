8e707450e313d00d53dd1c30f66aa5faa30feac0
Oleg Mojegov <olegmojegov@yandex.ru>
2021-06-15 22:31:43 +0300

    Add load logic movie in viewModel

--- ui-movie/src/main/java/com/example/ui_movie/movie/MovieViewModel.kt
+++ ui-movie/src/main/java/com/example/ui_movie/movie/MovieViewModel.kt
@@ -3,32 +3,38 @@ package com.example.ui_movie.movie
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.base.network.model.ConnectionType
+import com.example.themdb_api.MovieResult
+import com.example.themdb_api.UiMovie
 import com.example.themdb_api.themdbrepository.ThemdbRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
+
+private const val TOP_MORE = 14
 
 @HiltViewModel
 class MovieViewModel @Inject constructor(
     network: StateFlow<ConnectionType>,
     private val themdbRepository: ThemdbRepository
-): ViewModel() {
+) : ViewModel() {
+
+    private val _movieState = MutableStateFlow<MovieState>(MovieState.Loading())
+    val movieState: StateFlow<MovieState> = _movieState.asStateFlow()
 
     init {
+        _movieState.value = MovieState.Loading()
         viewModelScope.launch {
             network
                 .onEach {
-                    when(it) {
+                    when (it) {
                         is ConnectionType.Available -> {
-                            themdbRepository.getListGenresByMovie()
+                            loadGenres()
                             Timber.i("Connection type Available")
                         }
                         is ConnectionType.Lost -> {
+                            _movieState.value = MovieState.Loading()
                             Timber.i("Connection type lost")
                         }
                         else -> {
@@ -36,8 +42,38 @@ class MovieViewModel @Inject constructor(
                         }
                     }
                 }
-                .catch { }
                 .launchIn(this)
         }
+    }
+
+    fun loadGenres() {
+        viewModelScope.launch {
+            val genre = themdbRepository.getListGenresByMovie()
+            val popularity = themdbRepository.getPopularityMove()
+            val topPopularity = selectionTopMore(popularity.result, 5)
+            val listUiMovie = mutableListOf<UiMovie>()
+            genre.genres.forEach {
+                val movieApi = themdbRepository.getMovieByGenres(it.id.toString())
+                val topList = selectionTopMore(movieApi.result, TOP_MORE)
+                val uiMovie = UiMovie(it.name, topList)
+                listUiMovie.add(uiMovie)
+            }
+            if (listUiMovie.isNotEmpty()) {
+                _movieState.value = MovieState.Result(listUiMovie, UiMovie("Top", topPopularity))
+            } else {
+                _movieState.value = MovieState.ConnectionLost
+            }
+        }
+    }
+
+    private fun selectionTopMore(list: List<MovieResult>, countElement: Int): List<MovieResult> {
+        val editList = list.toMutableList()
+        editList.sortByDescending { it.voteCount }
+        return editList.take(countElement).toMutableList()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Timber.i("Clear")
     }
 }
