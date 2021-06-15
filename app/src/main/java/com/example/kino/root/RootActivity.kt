88989577043cea4a81b48aeba5f512d1ec40bdef
Денис Бекк <denisbeck555@gmail.com>
2021-06-15 20:04:03 +0300

    Update RootActivity.kt
    Override onCreate fun.

--- app/src/main/java/com/example/kino/root/RootActivity.kt
+++ app/src/main/java/com/example/kino/root/RootActivity.kt
@@ -1,8 +1,15 @@
 package com.example.kino.root
 
+import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.fragment.app.FragmentActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RootActivity: FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {  }
+    }
 }
