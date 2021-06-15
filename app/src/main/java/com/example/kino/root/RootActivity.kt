113581e446f4b676600a825c980b245156409186
Oleg Mojegov <olegmojegov@yandex.ru>
2021-06-15 22:51:06 +0300

    Fix package in Root.kt and fix scaffold

--- app/src/main/java/com/example/kino/root/RootActivity.kt
+++ app/src/main/java/com/example/kino/root/RootActivity.kt
@@ -3,7 +3,6 @@ package com.example.kino.root
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.fragment.app.FragmentActivity
-import com.example.kino.Root
 import com.example.ui_common_compose.theme.KinoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
