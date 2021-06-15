0fb3106e5a7368786523444b299ee8023830016d
Денис Бекк <denisbeck555@gmail.com>
2021-06-15 20:02:57 +0300

    Update RootActivity.kt
    Added AndroidEntryPoint annotation.

--- app/src/main/java/com/example/kino/root/RootActivity.kt
+++ app/src/main/java/com/example/kino/root/RootActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kino.root
 
 import androidx.fragment.app.FragmentActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RootActivity: FragmentActivity() {
 }
