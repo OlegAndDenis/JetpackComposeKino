3a9eb70c6daaa677604261f0e13e4063fdc8441d
Денис Бекк <denisbeck555@gmail.com>
2021-06-16 00:20:26 +0300

    Add Login.kt

---
+++ ui-login/src/main/java/com/example/ui_login/Login.kt
@@ -0,0 +1,27 @@
+package com.example.ui_login
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Login() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "KINO",
+            style = MaterialTheme.typography.h1,
+            color = MaterialTheme.colors.secondary,
+            modifier = Modifier.weight(0.34f)
+        )
+    }
+
+}
