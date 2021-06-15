31331949cc35e866a71b357fb1789a7caa0d57ed
Денис Бекк <denisbeck555@gmail.com>
2021-06-15 23:45:36 +0300

    Add ui-login module

---
+++ ui-login/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+android {
+    compileSdk = AppVersion.compileSdkVersion
+    buildToolsVersion = AppVersion.buildToolsVersion
+
+    defaultConfig {
+        minSdk = AppVersion.minSdkVersion
+        targetSdk = AppVersion.targetSdkVersion
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = AppVersion.targetJvm
+    }
+}
+
+dependencies {
+}
