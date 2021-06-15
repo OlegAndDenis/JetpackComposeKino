74543b2e4abc30b27203916842d06cb1477975b2
Денис Бекк <denisbeck555@gmail.com>
2021-06-15 20:11:03 +0300

    Update BottomNavBar.kt
    Added items for bottom nav bar.

--- app/src/main/java/com/example/kino/navigation/BottomNavBar.kt
+++ app/src/main/java/com/example/kino/navigation/BottomNavBar.kt
@@ -2,3 +2,10 @@ package com.example.kino.navigation
 
 // https://developer.android.com/jetpack/compose/navigation#bottom-nav
 
+/** Bottom navigation bar items */
+private val items = listOf(
+    RootScreen.Main,
+    RootScreen.Following,
+    RootScreen.Search,
+    RootScreen.Profile,
+)
