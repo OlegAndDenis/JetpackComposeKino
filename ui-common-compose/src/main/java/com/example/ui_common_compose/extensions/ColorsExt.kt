4fdceab0b8bfe044d3bfe061e67d538c861cce0e
Денис Бекк <denisbeck555@gmail.com>
2021-06-17 09:33:45 +0300

    Update ColorsExt.kt
    Renamed TextFieldColor to TextField.

--- ui-common-compose/src/main/java/com/example/ui_common_compose/extensions/ColorsExt.kt
+++ ui-common-compose/src/main/java/com/example/ui_common_compose/extensions/ColorsExt.kt
@@ -5,5 +5,5 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.example.ui_common_compose.theme.BlackBrown
 
-val Colors.TextFieldColor: Color
+val Colors.TextField: Color
     @Composable get() = if (isLight) Color.White else BlackBrown
