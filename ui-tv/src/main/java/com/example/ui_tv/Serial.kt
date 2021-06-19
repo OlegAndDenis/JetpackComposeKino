49afb60448cb58e9efd06f8cd59961e80777a6f8
Oleg Mojegov <olegmojegov@yandex.ru>
2021-06-19 18:54:21 +0300

    Add get state for serial

--- ui-tv/src/main/java/com/example/ui_tv/Serial.kt
+++ ui-tv/src/main/java/com/example/ui_tv/Serial.kt
@@ -1,8 +1,23 @@
 package com.example.ui_tv
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.ui_common_compose.extensions.rememberFlowWithLifecycle
 
 @Composable
 fun Serial() {
+    val viewModule: SerialViewModule = hiltViewModel()
+    viewModule.loadGenres()
+    val state = rememberFlowWithLifecycle(flow = viewModule.serials)
+        .collectAsState(initial = SerialState.Loading).value
+    when(state) {
+        is SerialState.Loading -> { }
+        is SerialState.Result -> {
 
+        }
+        else -> {
+
+        }
+    }
 }
