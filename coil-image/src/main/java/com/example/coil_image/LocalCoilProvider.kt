e50c953e2a47490c671a56a4291f8184747adfe6
Oleg Mojegov <olegmojegov@yandex.ru>
2021-06-26 13:49:47 +0300

    Fix loading image. Add single load

---
+++ coil-image/src/main/java/com/example/coil_image/LocalCoilProvider.kt
@@ -0,0 +1,17 @@
+package com.example.coil_image
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.platform.LocalContext
+import coil.ImageLoader
+import coil.imageLoader
+
+val LocalCoilImageLoader = staticCompositionLocalOf<ImageLoader?> { null }
+
+internal object LocalCoilProvider {
+
+    @Composable
+    fun getCoilImageLoader(): ImageLoader {
+        return LocalCoilImageLoader.current ?: LocalContext.current.imageLoader
+    }
+}
