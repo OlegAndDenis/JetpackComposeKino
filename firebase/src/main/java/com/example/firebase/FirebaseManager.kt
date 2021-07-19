3ba999ec9c3b48679c36f3cc42deafb96dba76c8
Денис Бекк <denisbeck555@gmail.com>
2021-07-19 23:16:39 +0300

    Update FirebaseManager.kt
    Added createUser fun.

--- firebase/src/main/java/com/example/firebase/FirebaseManager.kt
+++ firebase/src/main/java/com/example/firebase/FirebaseManager.kt
@@ -1,5 +1,32 @@
 package com.example.firebase
 
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import timber.log.Timber
+
 object FirebaseManager {
+    private val auth = Firebase.auth
+
+    fun createUser(
+        email: String,
+        password: String,
+        onSuccess: ((FirebaseUser) -> Unit) = {},
+        onError: ((Exception?) -> Unit) = {}
+    ) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    auth.currentUser?.let {
+                        Timber.d("createUserWithEmail:success")
+                        onSuccess(it)
+                    } ?: onError(Exception("User is null"))
+                } else {
+                    Timber.e(task.exception, "createUserWithEmail:failure")
+                    onError(task.exception)
+                }
+            }
+    }
+
 }
 
